31ad0d2f7a3f9db6715687bc289f092a028125da
Pablo Guimera <pablo@guimera.es>
2014-09-23 12:33:16 +0100

    Updated the login view

--- client/js/events.js
+++ client/js/events.js
@@ -1,1 +1,15 @@
-console.log('It works!');
+
+Template.login.events({
+  'click #create-init': function() {
+      var registerButtons = '<button id="create-cancel" type="button" class="btn btn-default">Cancel</button><button type="button" class="btn btn-default">Create</button>';
+      $('#button-switch').html(registerButtons);
+      $('#create-init').hide();
+      $('#create-pass').removeClass('hidden');
+  },
+  'click #create-cancel': function() {
+      var loginButtons = '<button type="button" class="btn btn-default">Login</button>';
+      $('#button-switch').html(loginButtons);
+      $('#create-init').show();
+      $('#create-pass').addClass('hidden');
+  }
+});
