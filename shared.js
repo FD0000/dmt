486f937b823821c5322d54cc00cf5edd64e43d8f
Momchil Gorchev <mgorchev@powa.com>
2014-12-04 15:35:46 +0000

    404 page

--- shared.js
+++ shared.js
@@ -3,23 +3,42 @@ Log = new Meteor.Collection('log');
 
 Router.configure({
     layoutTemplate: "mainLayout",
-    loadingTemplate: "loading" //TODO implement loading template
-//    notFoundTemplate: "notFound" //TODO implement notFound template
+    loadingTemplate: "loading",
+    notFoundTemplate: "missing"
 });
 
-//Router.onBeforeAction('loading');
-
-Router.map(function() {
-    this.route('home', {
-        path: '/',
-        waitOn: function() {
-            if (!(Meteor.loggingIn() || Meteor.user())) {
-                this.redirect("login");
-            }
+/**
+ * Controllers
+ */
+// Admin
+AdminAccessController = RouteController.extend({
+    waitOn: function() {
+        var currentUser = Meteor.user();
+        if (!(Meteor.loggingIn() || currentUser) || !(Roles.userIsInRole(currentUser, ['admin']))) {
+            this.redirect("login");
+        }
+        Meteor.subscribe('devices');
+        Meteor.subscribe('users');
+        Meteor.subscribe('log');
+    }
+});
+// User
+UserAccessController = RouteController.extend({
+    waitOn: function(){
+        if (!(Meteor.loggingIn() || Meteor.user())) {
+            this.redirect("login");
+        } else{
             Meteor.subscribe('devices');
             Meteor.subscribe('users');
         }
-    });
+    }
+});
+
+/**
+ * Routes definition
+ */
+Router.map(function() {
+
     this.route('login', {
         path: 'login',
         waitOn: function() {
@@ -28,95 +47,44 @@ Router.map(function() {
             }
         }
     });
+
+    // Routes requiring admin privileges
+    this.route('logs', {
+        path: 'logs',
+        template:'logs',
+        controller: AdminAccessController
+    });
+    this.route('users', {
+        path: 'users',
+        template:'users',
+        controller: AdminAccessController
+    });
+    this.route('stats', {
+        path: 'stats',
+        template:'stats',
+        controller: AdminAccessController
+    });
     this.route('add-device', {
         path: 'add-device',
-        waitOn: function() {
-            var currentUser = Meteor.user();
-            if (!(Meteor.loggingIn() || currentUser) || !(Roles.userIsInRole(currentUser, ['admin']))) {
-                this.redirect("login");
-            }
-            Meteor.subscribe('devices');
-            Meteor.subscribe('users');
-        }
+        controller: AdminAccessController
+    });
+
+    //Routes requiring login in
+    this.route('home', {
+        path: '/',
+        controller: UserAccessController
     });
     this.route('book', {
         path: 'book/:_DeviceId',
         data: function(){
             return Devices.findOne({_id: this.params._DeviceId});
         },
-        waitOn: function() {
-            if (!(Meteor.loggingIn() || Meteor.user())) {
-                this.redirect("login");
-            }
-            Meteor.subscribe('devices');
-            Meteor.subscribe('users');
-            Meteor.subscribe('log-by-id', this.params._DeviceId);
-        }
-
+        controller: UserAccessController
     });
     this.route('bookedByMe', {
         path: 'booked-By-Me',
         template: 'bookedByMe',
-        waitOn: function(){
-            if (!(Meteor.loggingIn() || Meteor.user())) {
-                this.redirect("login");
-            } else{
-                Meteor.subscribe('devices');
-                Meteor.subscribe('users');
-            }
-        }
+        controller: UserAccessController
+    });
 
-    });
-    this.route('logs', {
-       path: 'logs',
-       template:'logs',
-       waitOn: function() {
-           var currentUser = Meteor.user();
-           if (!(Meteor.loggingIn() || currentUser) || !(Roles.userIsInRole(currentUser, ['admin']))) {
-               this.redirect("login");
-           }
-           Meteor.subscribe('devices');
-           Meteor.subscribe('users');
-           Meteor.subscribe('log');
-       }
-    });
-    this.route('users', {
-        path: 'users',
-        template:'users',
-        waitOn: function() {
-            var currentUser = Meteor.user();
-            if (!(Meteor.loggingIn() || currentUser) || !(Roles.userIsInRole(currentUser, ['admin']))) {
-                this.redirect("login");
-            }
-            Meteor.subscribe('devices');
-            Meteor.subscribe('users');
-            Meteor.subscribe('log');
-        }
-    });
-    this.route('stats', {
-        path: 'stats',
-        template:'stats',
-        waitOn: function() {
-            var currentUser = Meteor.user();
-            if (!(Meteor.loggingIn() || currentUser) || !(Roles.userIsInRole(currentUser, ['admin']))) {
-                this.redirect("login");
-            }
-            Meteor.subscribe('devices');
-            Meteor.subscribe('users');
-            Meteor.subscribe('log');
-        }
-    });
-    this.route('admin', {
-        path: 'admin',
-        waitOn: function() {
-            if (!(Meteor.loggingIn() || Meteor.user())) {
-                this.redirect("login");
-            }
-            else if(!Roles.userIsInRole(Meteor.user(), ['admin'])){
-                this.redirect('/');
-            }
-            Meteor.subscribe('devices');
-            Meteor.subscribe('users');
-        }
-    });
 });
