d5f6c56ee8ca615e3656f859b830c9f5f4c20fb8
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-03-24 17:03:34 +0000

    Client side tests

--- tests/mocha/client/sampleClientTest.js
+++ tests/mocha/client/sampleClientTest.js
@@ -1,9 +1,30 @@
-if (!(typeof MochaWeb === 'undefined')){
-  MochaWeb.testOnly(function(){
-    describe("a group of tests", function(){
-      it("should respect equality", function(){
-        chai.assert.equal(5,5);
-      });
-    });
-  });
+if(Meteor.isClient){
+    if (!(typeof MochaWeb === 'undefined')){
+        MochaWeb.testOnly(function(){
+
+            var expect = chai.expect;
+
+            describe("Client initialisation", function(){
+                it('Initialise utility functions', function(){
+                    expect(validateDataObject).to.be.an('function');
+                    expect(validateRegister).to.be.an('function');
+                });
+            });
+
+            describe('Template initialisation', function(){
+                var temp = [
+                    'home', 'deviceGrid', 'mainLayout', 'bookedDeviceGrid',
+                    'admin', 'bookedByMe', 'logs', 'users', 'feedback'
+                ];
+                temp.forEach(function(item) {
+                    it('Initialise template: ' + item, function() {
+                        expect(Template[item]).to.be.an('object');
+                    });
+                    it('Initialise Template.'+ item +' helpers', function(){
+                        expect(Template[item].helpers).to.be.an('function');
+                    })
+                });
+            });
+        });
+    }
 }
