7d3befd0f1abe449eda263ad980994e3ef645c0c
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-08-16 23:49:28 +0100

    Disabling account creation

--- apm.js
+++ apm.js
@@ -1,7 +1,3 @@
-//document.addEventListener('DOMContentLoaded', function(e){
-//  console.log('Hello world!');
-//});
-//
-//var bigData = [];
-//var user = Meteor.user()._id;
-//bigData.push(user);
+Accounts.config({
+    forbidClientAccountCreation : true
+});
